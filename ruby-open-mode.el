2a69f890abe0194132bb7b3f77ca8514736d1d57
tomykaira <tomykaira@gmail.com>
2012-05-06 13:46:58 +0900

    open test if exists

--- ruby-open-mode.el
+++ ruby-open-mode.el
@@ -24,4 +24,36 @@
 
 (define-open-mode "ruby")
 
+(defun ruby-open-cand-spec (cand-file)
+  (let ((cand-file-name (car (last (split-string cand-file "/")))))
+    (message cand-file-name)
+    (if (string-match "\.rb$" cand-file-name)
+        (let ((fn (concat "/" (car (split-string cand-file-name "\\."))
+                         "_spec.rb"))
+              (files (om--subdirectory-files om--temporary-project-root "spec")))
+          (car (delq nil (mapcar (lambda (s) (and (numberp (string-match (regexp-quote fn) s)) s)) files)))))))
+
+(defun ruby-open-anything-c-open-candidate-in-new-screen (cand-file)
+  (let* ((candidate (concat om--temporary-project-root cand-file)) 
+         (existing-screen (elscreen-find-screen-by-file-path candidate)))
+    (cond
+     (existing-screen
+      (elscreen-goto existing-screen))
+     ((fboundp 'elscreen-create)
+      (elscreen-create)
+      (split-window-horizontally)
+      (find-file candidate)
+      (let ((cand-spec (ruby-open-cand-spec cand-file)))
+        (when cand-spec
+          (other-window 1)
+          (find-file (concat om--temporary-project-root cand-spec))))))))
+
+(defun ruby-open-anything (args)
+  (interactive "P")
+  (letf (((symbol-function 'om-anything-c-open-candidate-in-new-screen)
+          (symbol-function 'ruby-open-anything-c-open-candidate-in-new-screen)))
+    (anything-other-buffer
+     (om-make-anything-sources 'ruby-open-root ruby-open-ignored (if args 'other-window 'new-screen))
+     nil)))
+
 (provide 'ruby-open-mode)
